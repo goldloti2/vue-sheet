bedc195bdbf747f3ea3409178b1affe82b7b55b5
goldloti2 <goldloti2@gmail.com>
2026-08-27 11:43:41 +0800

    add TabBar UI

---
+++ vue-build/src/components/ui/TabBar.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+  defineProps<{
+    tabs: string[]
+  }>()
+
+  const model = defineModel<string>({ required: true })
+</script>
+
+<template>
+  <v-tabs v-model="model" align-tabs="center" grow>
+    <v-tab v-for="tab in tabs" :key="tab" :value="tab">{{ tab }}</v-tab>
+  </v-tabs>
+</template>
